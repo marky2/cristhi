0db40cb35448418f7127093e5d6d09c6d19b5d1a
behita <behatriz.13gnicasio@gmail.com>
2016-03-17 16:54:31 -0500

    edificios

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+</head>
+
+
+
+<body>
+	<div class="container">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">MARKI--EDIF </a>
+				</div>
+		
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Precios</a></li>
+						<li><a href="#">Ofertas</a></li>
+					</ul>
+					<form class="navbar-form navbar-left" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="Direccion">
+						</div>
+						<button type="submit" class="btn btn-default">Entrar</button>
+					</form>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">LUGARES</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Elige el lugar  que mas te agrade<b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Lima</a></li>
+								<li><a href="#">Piura</a></li>
+								<li><a href="#">Mancora</a></li>
+								<li><a href="#">Arequipa</a></li>
+								<li><a href="#">Chiclayo</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
+
+		<!-- CAROUSEL -->
+		
+		<div id="carousel-id" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="2" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="3" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="4" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="5" class=""></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="item">
+					<img data-src="public/imagenes/1.jpg" alt="First slide" src="public/imagenes/1.jpg">
+					
+				</div>
+				<div class="item">
+					<img data-src="public/imagenes/2.jpg" src="public/imagenes/2.jpg">
+					
+				</div>
+				<div class="item active">
+					<img data-src="public/imagenes/3.jpg" alt="Third slide" src="public/imagenes/3.jpg">
+					
+				</div>
+				<div class="item">
+					<img data-src="public/imagenes/4.jpg" alt="First slide" src="public/imagenes/4.jpg">
+					
+				</div>
+				<div class="item">
+					<img data-src="public/imagenes/5.jpg" alt="First slide" src="public/imagenes/5.jpg">
+					</div>
+				<div class="item">
+					<img data-src="public/imagenes/6.jpg" alt="First slide" src="public/imagenes/6.jpg">
+					
+				</div>
+
+			</div>
+			<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+			<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+		</div>
+
+	</div><br>
+
+
+
+	<script src="public/js/jquery-1.12.1.js"></script>
+	<script src="public/js/bootstrap.js"></script>
+
+	
+		</div>
+		<div id="carousel-id" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-id" data-slide-to="6" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="7" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="8" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="9" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="10" class=""></li>
+				<li data-target="#carousel-id" data-slide-to="11" class=""></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="item">
+					<img data-src="public/imagenes/7.jpg" alt="First slide" src="public/imagenes/7.jpg">
+					
+				</div>
+				<div class="item">
+					<img data-src="public/imagenes/8.jpg" src="public/imagenes/8.jpg">
+					
+				</div>
+				<div class="item active">
+					<img data-src="public/imagenes/9.jpg" alt="Third slide" src="public/imagenes/9.jpg">
+					
+				</div>
+				<div class="item">
+					<img data-src="public/imagenes/10.jpg" alt="First slide" src="public/imagenes/10.jpg">
+					
+				</div>
+				<div class="item">
+					<img data-src="public/imagenes/11.jpg" alt="First slide" src="public/imagenes/11.jpg">
+					</div>
+				<div class="item">
+					<img data-src="public/imagenes/12.jpg" alt="First slide" src="public/imagenes/12.jpg">
+					
+				</div>
+
+			</div>
+			<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+			<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+
+		</div>
